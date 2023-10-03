380317ad5395a82631c264e40ba11bdc38faa4b9
ian Kinnear <ian.kinnear@hotmail.com>
2023-10-03 16:45:12 +0100

    first commit

---
+++ portfolio/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/news")
+def news():
+    return render_template("news.html")
+
+@app.route("/fixtures")
+def fixtures():
+    return render_template("fixtures.html")
+
+@app.route("/form", methods=['GET', 'POST'])
+def form_submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        return f"Received data: Name - {name}, Email - {email}"
+
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
