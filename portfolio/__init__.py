85a671e57ec0ff15e2eaec5a25b75d263d8196e9
ian Kinnear <ian.kinnear@hotmail.com>
2023-10-03 18:37:21 +0100

    ok

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/index")
-def index():
-    return render_template("index.html")
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 @app.route("/contact")
 def contact():
